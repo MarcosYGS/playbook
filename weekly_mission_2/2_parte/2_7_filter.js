18226185d633685178a62bc2ed739e9dcaabf157
Marcos_yeyo <yeyomarcos@gmail.com>
2022-04-22 17:48:11 -0500

    arregalndo una variable

--- weekly_mission_2/2_parte/2_7_filter.js
+++ weekly_mission_2/2_parte/2_7_filter.js
@@ -1,6 +1,6 @@
 // Ejemplo 7: Uso de filter para filtrar una lista de elementos
-const countries7 = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
-const countriesContainingLand = countries7.filter((country) => // esta es una función
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+const countriesContainingLand = countries.filter((country) => // esta es una función
   country.includes('land') // indicación para solo filtrar elementos que incluyan "land"
 )
 console.log("Ejemplo 7: Uso de filter para filtrar una lista de elementos")
