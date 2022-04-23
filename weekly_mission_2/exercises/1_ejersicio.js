e83b401c0338842104b4ee600549e99bc8af39cc
Marcos_yeyo <yeyomarcos@gmail.com>
2022-04-22 22:29:53 -0500

    ejemplo y ejersicio 1

---
+++ weekly_mission_2/exercises/1_ejersicio.js
@@ -0,0 +1,23 @@
+
+
+const repo = {
+    
+    title: "02 Live 2 Semana 1 [Miércoles 6 de Abril]",
+    repositoryNameAssociated: "MissionNodeJS",
+    status:"open",
+    numberOfComments:115,
+    labels:["FINALIZADO", "LIVE 2", "Semana-1"],
+    author:"carlogilmar",
+    dateCreated:"6/abril/22",
+    lastUpdated:"6/abril/22",
+
+    getTitleAndAuthor: function(){
+      return `this issue: ${this.title} was created by ${this.author}`
+    },
+    getGeneralInfo: function(){
+      return `the labels are: ${this.labels}. it status is ${this.status}.`
+    }
+   }
+   
+   console.log(repo.getTitleAndAuthor())
+   console.log(repo.getGeneralInfo())
