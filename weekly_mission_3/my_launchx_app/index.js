fb809b64c18aa94a0ac04731b5dcbb319c3ee897
Marcos_yeyo <yeyomarcos@gmail.com>
2022-04-24 22:51:42 -0500

    primer ejercicio de la semana 3

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo0 = new MissionCommander("Carlo0")
+const carlo1 = new MissionCommander("Carlo1")
+const carlo2 = new MissionCommander("Carlo2")
+console.log(carlo0.name)
+console.log(carlo1.name)
+console.log(carlo2.name)
