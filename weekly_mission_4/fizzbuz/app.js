42d7b74b288eb376c1ecd85abe21823883c8e20c
Marcos_yeyo <yeyomarcos@gmail.com>
2022-05-10 23:48:29 -0500

    agregando primer paso

--- weekly_mission_4/fizzbuz/app.js
+++ weekly_mission_4/fizzbuz/app.js
@@ -1,8 +1,13 @@
-const fs = require("fs");
 
+const fs = require('fs');
 // Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
+const Reader = require("./lib/utils/Reader");
+const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
+console.log(explorers)
+
+
+
+
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
